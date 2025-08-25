7ace9e69e005cbf77b64db702a57c446dbe388b3
ngocvisas <htngoc2000@gmail.com>
2025-08-26 01:18:58 +0700

    Update db.ts

--- valorant/db.ts
+++ valorant/db.ts
@@ -1,1 +1,18 @@
+import { postgres } from "encore.dev/storage/postgres";
+export const db = postgres.NewDatabase("main", { migrations: "./migrations" });
+
+export async function getAgentById(id: string) {
+  const rows = await db.sql`
+    SELECT id, name, role, bio, image_url as "imageUrl"
+    FROM agents WHERE id=${id} LIMIT 1`;
+  return rows[0];
+}
+
+export async function getWeaponById(id: string) {
+  const rows = await db.sql`
+    SELECT id, name, type, cost, dmg_body as "dmgBody",
+           dmg_head as "dmgHead", dps, image_url as "imageUrl"
+    FROM weapons WHERE id=${id} LIMIT 1`;
+  return rows[0];
+}
 
